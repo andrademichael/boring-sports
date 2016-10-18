6063ef8f4128d8ab5ce476acd709b19bf24dfaca
Getro Naissance and Michael Andrade <getro@>
2016-10-18 13:20:46 -0700

    Added click to highlight functionality to paragraphs

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+  $("#pOne").click(function(){
+  $("p").removeClass();
+  $("#pOne").toggleClass("highlight");
+  });
+
+  $("#pTwo").click(function(){
+  $("p").removeClass();
+  $("#pTwo").toggleClass("highlight");
+  });
+
+  $("#pThree").click(function(){
+  $("p").removeClass();
+  $("#pThree").toggleClass("highlight");
+  });
+
+});
